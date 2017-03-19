beffe29397b49254ca1e4449b6c00beda723e9a5
Raditya Arya <arya.raditya26@gmail.com>
2017-03-19 11:43:44 +0700

    simple login page with fb auth

---
+++ components/LoginScene.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import FbButton from './Login/FbButton'
+import {
+  Container,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+} from 'native-base';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from 'react-native';
+
+var {width, height} = require('Dimensions').get('window');
+
+export default class Login extends Component {
+  render() {
+    return (
+      <Container>
+          <View style={styles.container}>
+            <View style={styles.logo}>
+              <Text style={styles.logoText}>
+                Logo Botram
+              </Text>
+            </View>
+
+            <View style={styles.button}>
+              <FbButton navigator={this.props.navigator}/>
+              <Button warning onPress={() => {
+                this.props.navigator.push({index:1})
+              }}>
+                <Text>
+                  button google
+                </Text>
+              </Button>
+            </View>
+          </View>
+      </Container>
+    )
+  }
+}
+
+var styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#b71c1c',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  logo: {
+    marginTop:height/8,
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+  logoText: {
+    color: '#FFFFFF',
+    fontSize: 30
+  },
+  button: {
+    margin:height/80,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  }
+})
--- index.android.js
+++ index.android.js
@@ -1,112 +1,54 @@
 import React, { Component } from 'react';
-import HomeScene from './components/Home'
-import FavoritesScene from './components/Favorites'
-import CameraScene from './components/Camera'
+import HomeScene from './components/Home';
+import FavoritesScene from './components/Favorites';
+import LoginScene from './components/LoginScene';
+import CameraScene from './components/Camera';
 import {
   AppRegistry,
   Navigator,
   BackAndroid,
-  TouchableOpacity,
-  Text,
-  View
 } from 'react-native';
-
-import FBSDK, { GraphRequest, GraphRequestManager, LoginManager, LoginButton, AccessToken } from 'react-native-fbsdk';
 
 export default class Botram extends Component {
 
-  // handlelogin(){
-  //   // fbAuth() {
-  //     LoginManager.logInWithReadPermissions(['public_profile']).then(function(result){
-  //       if (result.isCancelled) {
-  //         console.log('Login was cancelled');
-  //       }
-  //       else {
-  //         console.log('Login was success ' + result.grantedPermissions.toString());
-  //       }
-  //     },function(error){
-  //       console.log('An error occured: ' + error);
-  //     })
-  //   // }
-  // }
-
-
   renderNewScene (route, navigator) {
     if (route.index === 0) {
-      return (
-        <HomeScene route={route} navigator={navigator} />
+      return(
+        <LoginScene route={route} navigator={navigator} />
       )
     }
     else if (route.index === 1) {
       return (
-        <FavoritesScene route={route} navigator={navigator} />
+        <HomeScene route={route} navigator={navigator} />
       )
     }
     else if (route.index === 2) {
+      return (
+        <FavoritesScene route={route} navigator={navigator} />
+      )
+    }
+    else if (route.index === 3) {
+      return (
+        <PreCameraScene route={route} navigator={navigator} />
+      )
+    }
+    else if (route.index === 4) {
       return (
         <CameraScene route={route} navigator={navigator} />
       )
     }
   }
 
+
   render() {
+    const appThis = this
     return (
-      <View>
-        <LoginButton
-    onLoginFinished={
-      (error, result) => {
-        if (error) {
-          alert("login has error: " + result.error);
-        } else if (result.isCancelled) {
-          alert("login is cancelled.");
-        } else {
-
-          AccessToken.getCurrentAccessToken().then(
-            (data) => {
-              let accessToken = data.accessToken
-              alert(accessToken.toString())
-
-              const responseInfoCallback = (error, result) => {
-                if (error) {
-                  console.log(error)
-                  alert('Error fetching data: ' + error.toString());
-                } else {
-                  console.log(result)
-                  alert('Success fetching data: ' + result.toString());
-                }
-              }
-
-              const infoRequest = new GraphRequest(
-                '/me',
-                {
-                  accessToken: accessToken,
-                  parameters: {
-                    fields: {
-                      string: 'email,name,first_name,middle_name,last_name,birthday,picture'
-                    }
-                  }
-                },
-                responseInfoCallback
-              );
-
-              // Start the graph request.
-              new GraphRequestManager().addRequest(infoRequest).start()
-
-            }
-          )
-
-        }
-      }
-    }
-    onLogoutFinished={() => alert("logout.")}/>
-
-      </View>
-      // <Navigator
-      //   initialRoute={{index: 0 }}
-      //   renderScene={appThis.renderNewScene}
-      //   configureScene={(route, routeStack) =>
-      //     Navigator.SceneConfigs.FadeAndroid}
-      // />
+      <Navigator
+        initialRoute={{index: 0 }}
+        renderScene={appThis.renderNewScene}
+        configureScene={(route, routeStack) =>
+          Navigator.SceneConfigs.FadeAndroid}
+      />
     )
   }
 }
