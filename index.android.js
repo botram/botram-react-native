5ac03e89592b766e737016e69dc4f4c778a28a42
YOMA <yaumashf@gmail.com>
2017-03-22 14:26:11 +0700

    remake detail page foods

--- index.android.js
+++ index.android.js
@@ -91,7 +91,7 @@ export default class Botram extends Component {
     const appThis = this;
     return (
       <Navigator
-        initialRoute={{title: 'PostingFoodScene' }}
+        initialRoute={{title: 'FoodDetails' }}
         renderScene={appThis.renderNewScene}
         configureScene={(route, routeStack) =>
           Navigator.SceneConfigs.FadeAndroid}
