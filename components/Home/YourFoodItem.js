b4d79468250fbe5f8c74a312308a98d568c6ad43
YOMA <yaumashf@gmail.com>
2017-03-23 15:02:37 +0700

    wiring my foods, pic, name,stok,price

--- components/Home/YourFoodItem.js
+++ components/Home/YourFoodItem.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text, Image, ScrollView, Alert, } from 'react-native';
+import { View, Text, Image, ScrollView, Alert, AsyncStorage, } from 'react-native';
 import { Container, Content, Card, CardItem, Left, Body, Thumbnail, Button, Right } from 'native-base'
 import { Col, Row, Grid } from "react-native-easy-grid";
 import Icon from 'react-native-vector-icons/MaterialIcons'
-import food1 from '../../dummyFiles/food1.jpg'
-import food2 from '../../dummyFiles/food2.jpg'
-import food3 from '../../dummyFiles/food3.jpg'
-import food4 from '../../dummyFiles/food4.jpg'
 
 var {width, height} = require('Dimensions').get('window');
 
@@ -17,8 +13,33 @@ export default class FoodItemCard extends Component {
       showhide  : 'Hide',
       activePicture : 1,
       status : 'Available',
+      myfood : [],
     }
   }
+
+  componentWillMount() {
+    AsyncStorage.getItem("userId").then(userId => {
+      AsyncStorage.getItem("token").then(token => {
+        if(!token) {
+          this.props.navigator.resetTo({title:'LoginScene'});
+        } else {
+          fetch(`http://botram-api-dev.ap-southeast-1.elasticbeanstalk.com/api/users/food/byuser/${userId}`, {
+            method: 'GET',
+            headers: {
+              token: token
+            }
+          })
+          .then(res => {
+            return res.json()
+          })
+          .then(data => {
+            this.setState ({myfood: data })
+          }).catch(err => console.log(err))
+        }
+      })
+    })
+  }
+
   handleStatus(){
     if (this.state.showhide==='Hide') {
       this.setState({
@@ -37,22 +58,24 @@ export default class FoodItemCard extends Component {
 
   }
   render(){
+    console.log(this.state.myfood);
     return(
       <Container>
         <ScrollView>
-          <Content>
+          {this.state.myfood.map((data,index) => {
+            return (<Content key={index}>
             <Card>
               <CardItem cardBody>
-                <Image style={{ opacity:this.state.activePicture, backgroundColor: '#000000', resizeMode: 'cover', width: width, height: height/3 }} source={food1}/>
+                <Image style={{ opacity:this.state.activePicture, backgroundColor: '#000000', resizeMode: 'cover', width: width, height: height/3 }} source={{uri:data.food_pic}}/>
               </CardItem>
               <CardItem>
                 <Grid>
                   <Row style={{marginBottom: height/100}}>
-                  <Col size={77}><Text style={{color: '#282828', marginRight: width/100, fontSize: width/22, fontWeight: 'bold'}}>Cilok</Text></Col>
-                  <Col size={23}><Text style={{color: '#282828'}}>Rp 15.000</Text></Col>
+                  <Col size={77}><Text style={{color: '#282828', marginRight: width/100, fontSize: width/22, fontWeight: 'bold'}}>{data.food_title}</Text></Col>
+                  <Col size={23}><Text style={{color: '#282828'}}>Rp {data.food_price}</Text></Col>
                   </Row>
                   <Row>
-                  <Col size={77}><Text style={{fontSize: width/27, color: '#6C7A89'}}>Stok : 7 Porsi</Text></Col>
+                  <Col size={77}><Text style={{fontSize: width/27, color: '#6C7A89'}}>Stok : {data.food_qty} Porsi</Text></Col>
                   <Col size={23}>
                     <Button
                       onPress={() => Alert.alert(
@@ -92,53 +115,10 @@ export default class FoodItemCard extends Component {
                 </Grid>
               </CardItem>
             </Card>
-          </Content>
-          <Content>
-            <Card>
-              <CardItem cardBody>
-                <Image style={{ resizeMode: 'cover', width: width, height: height/3 }} source={food4}/>
-              </CardItem>
-              <CardItem>
-                <Grid>
-                  <Row style={{marginBottom: height/100}}>
-                  <Col size={77}><Text style={{color: '#282828', marginRight: width/100, fontSize: width/22, fontWeight: 'bold'}}>Martabak Telor</Text></Col>
-                  <Col size={23}><Text style={{color: '#282828'}}>Rp 30.000</Text></Col>
-                  </Row>
-                  <Row>
-                  <Col size={77}><Text style={{fontSize: width/27, color: '#6C7A89'}}>Stok : 12 Porsi</Text></Col>
-                  <Col size={23}>
-                    <Button style={{justifyContent:'center',width: width/5, height: height/25, backgroundColor: '#00B16A'}}>
-                      <Text style={{color: '#FFFFFF'}}>Edit</Text>
-                    </Button>
-                   </Col>
-                  </Row>
-                </Grid>
-              </CardItem>
-            </Card>
-          </Content>
-          <Content>
-            <Card>
-              <CardItem cardBody>
-                <Image style={{ resizeMode: 'cover', width: width, height: height/3 }} source={food3}/>
-              </CardItem>
-              <CardItem>
-                <Grid>
-                  <Row style={{marginBottom: height/100}}>
-                  <Col size={77}><Text style={{color: '#282828', marginRight: width/100, fontSize: width/22, fontWeight: 'bold'}}>Martabak Manis</Text></Col>
-                  <Col size={23}><Text style={{color: '#282828'}}>Rp 22.000</Text></Col>
-                  </Row>
-                  <Row>
-                  <Col size={77}><Text style={{fontSize: width/27, color: '#6C7A89'}}>Stok : 10 Porsi</Text></Col>
-                  <Col size={23}>
-                    <Button style={{justifyContent:'center',width: width/5, height: height/25, backgroundColor: '#00B16A'}}>
-                      <Text style={{color: '#FFFFFF'}}>Edit</Text>
-                    </Button>
-                   </Col>
-                  </Row>
-                </Grid>
-              </CardItem>
-            </Card>
-          </Content>
+          </Content>)
+
+          })}
+
         </ScrollView>
       </Container>
         );
