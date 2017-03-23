f20aeeba6e809f019734ead964527992776732df
YOMA <yaumashf@gmail.com>
2017-03-23 20:26:34 +0700

    refactor home

--- components/Home.js
+++ components/Home.js
@@ -1,18 +1,17 @@
+import SideBar from './MenuDrawer';
+import { connect } from 'react-redux';
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
-import { fetchTimeline } from '../action/food_action'
-import { connect } from 'react-redux';
-
+import FoodItemCard from './Home/FoodItemCard';
+import { fetchTimeline } from '../action/food_action';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import {
   Container,
-  Content,
   Footer,
   FooterTab,
   Drawer,
   Button,
 } from 'native-base';
-import SideBar from './MenuDrawer';
-import { Col, Row, Grid } from "react-native-easy-grid";
 import {
   StyleSheet,
   Text,
@@ -23,9 +22,12 @@ import {
   TouchableOpacity,
   AsyncStorage,
 } from 'react-native'
-var {width, height} = require('Dimensions').get('window');
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import FoodItemCard from './Home/FoodItemCard'
+import {
+  Col,
+  Row,
+  Grid
+} from "react-native-easy-grid";
+
 var {width, height} = require('Dimensions').get('window');
 const iconmenu = (<Icon name="menu" size={30} color="#FFFFFF" />)
 const iconhome = (<Icon name="home" size={30} color="#b71c1c" />)
@@ -72,64 +74,64 @@ class Home extends Component {
     const appthis = this;
     setTimeout(function() { appthis.setState({klik: 1});}.bind(appthis), 2000);
   }
-    render() {
-      const appthis = this;
-      BackAndroid.addEventListener('hardwareBackPress', () => {
-        if (appthis.state.klik===2) {
-          BackAndroid.exitApp()
-          return false
-        }
-        else if (appthis.state.klik===1) {
-          appthis.props.navigator.popToTop()
-          appthis.handleExitBack()
-          return true;
-        }
-      })
-        return (
-          <Drawer
-            ref={(ref) => { this._drawer = ref; }}
-            content={<SideBar navigator={this._navigator} nav={this.props.navigator} />}
-            onClose={() => this.closeDrawer()}
-            style={{width:100}}
-          >
-            <Row style={{height: 50,backgroundColor:'#B71C1C',}}>
-             <Col size={35}>
-               <TouchableOpacity  onPress={() => {this.openDrawer()}}>
-                 <Text style={{padding:10,}}>{iconmenu}</Text>
-               </TouchableOpacity>
-             </Col>
-             <Col size={65}><Text style={{padding:10,color: '#FFFFFF',fontSize:22, }}>Botram</Text></Col>
-            </Row>
-            <Container>
-              <View style={styles.container}>
-                <FoodItemCard navigator={this.props.navigator} data={this.props.foodList}/>
-              </View>
-              <Footer >
-                <FooterTab style={{backgroundColor:'#ECECEC',}}>
-                  <Button onPress={() => {
-                    this.props.navigator.push({title:'FavoritesScene'})
-                  }}>
-                    {iconfav}
-                  </Button>
-                  <Button >
-                    {iconhome}
-                  </Button>
-                  <Button onPress={() => {
-                    this.props.navigator.push({title:'SearchScene'})
-                  }}>
-                  {iconSearch}
-                  </Button>
-                  <Button onPress={() => {
-                    this.props.navigator.push({title:'CameraScene'})
-                  }}>
-                    {iconcamera}
-                  </Button>
-                </FooterTab>
-              </Footer>
-            </Container>
-          </Drawer>
-        );
-    }
+  render() {
+    const appthis = this;
+    BackAndroid.addEventListener('hardwareBackPress', () => {
+      if (appthis.state.klik===2) {
+        BackAndroid.exitApp()
+        return false
+      }
+      else if (appthis.state.klik===1) {
+        appthis.props.navigator.popToTop()
+        appthis.handleExitBack()
+        return true;
+      }
+    })
+      return (
+        <Drawer
+          ref={(ref) => { this._drawer = ref; }}
+          content={<SideBar navigator={this._navigator} nav={this.props.navigator} />}
+          onClose={() => this.closeDrawer()}
+          style={{width:100}}
+        >
+          <Row style={{height: 50,backgroundColor:'#B71C1C',}}>
+           <Col size={35}>
+             <TouchableOpacity  onPress={() => {this.openDrawer()}}>
+               <Text style={{padding:10,}}>{iconmenu}</Text>
+             </TouchableOpacity>
+           </Col>
+           <Col size={65}><Text style={{padding:10,color: '#FFFFFF',fontSize:22, }}>Botram</Text></Col>
+          </Row>
+          <Container>
+            <View style={styles.container}>
+              <FoodItemCard navigator={this.props.navigator} data={this.props.foodList}/>
+            </View>
+            <Footer >
+              <FooterTab style={{backgroundColor:'#ECECEC',}}>
+                <Button onPress={() => {
+                  this.props.navigator.push({title:'FavoritesScene'})
+                }}>
+                  {iconfav}
+                </Button>
+                <Button >
+                  {iconhome}
+                </Button>
+                <Button onPress={() => {
+                  this.props.navigator.push({title:'SearchScene'})
+                }}>
+                {iconSearch}
+                </Button>
+                <Button onPress={() => {
+                  this.props.navigator.push({title:'CameraScene'})
+                }}>
+                  {iconcamera}
+                </Button>
+              </FooterTab>
+            </Footer>
+          </Container>
+        </Drawer>
+      );
+  }
 }
 
 const mapsStateToProps = (state) =>{
